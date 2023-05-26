21faccc99a21d7b881d45fc54b2991ed45a99734
Timafe <23timahall@students.mvsd320.org>
2023-05-26 13:11:14 -0700

    Completed Codes for the programming final

--- Assets/Final Exam/#1/Car.cs
+++ Assets/Final Exam/#1/Car.cs
@@ -12,24 +12,17 @@ public class Car : MonoBehaviour
     private float verticalInput;
     private float horizontalInput;
 
-
-    // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         sr = GetComponent<SpriteRenderer>();
     }
 
-   
     void Update()
     {
-
-        /*
-         * FINAL EXAM #1
-         * Write a script that will allow movement.
-         * The Vertical Axis should make the player move forwards and backwards
-         * The Horizontal Axis should make the player rotate on the Vector3.back axis
-         */
-
+     verticalInput = Input.GetAxis("Vertical");
+     horizontalInput = Input.GetAxis("Horizontal");
+     rb.velocity = transform.up * verticalInput * moveSpeed;
+     rb.angularVelocity = -horizontalInput * turnSpeed;
     }
 }
--- Assets/Final Exam/#3/SpeedPickup.cs
+++ Assets/Final Exam/#3/SpeedPickup.cs
@@ -2,23 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-/*
- * Final Exam #3
- * 
- *  Make this script inherit the PickUp Class.
- * Attach this script to the yellow arrow that is on the track
- * Override the Activate() method to:
- *      - Save the original speed of the player
- *      - increase the speed of the car to 5
- *      - Wait for 2 seconds (Write a coroutine)
- *      - Change the speed back to the originalSpeed
- * 
- */
-public class SpeedPickup : MonoBehaviour
+public class SpeedPickup : PickUp
 {
     public float originalSpeed;
-    public float boostedSpeed;
+    public float boostedSpeed = 5f;
 
+    public override void Activate()
+    {
+        originalSpeed = player.moveSpeed; 
+        player.moveSpeed = boostedSpeed;
 
+        StartCoroutine(ResetSpeedAfterDelay());
+    }
+
+    private IEnumerator ResetSpeedAfterDelay()
+    {
+        yield return new WaitForSeconds(2f);
+        player.moveSpeed = originalSpeed;
+    }
 }
